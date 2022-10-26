e555b8a856fd7bffbf17d5e9c46e430c99910693
Duknam Yoo <yoo2001818@gmail.com>
2022-10-26 14:51:45 +0900

    feat: chalk, ora 추가 (#3)

---
+++ .changeset/wild-rockets-attack.md
@@ -0,0 +1,5 @@
+---
+"i18next-google-sheet": minor
+---
+
+ESM 사용하도록 패키지 구성 수정
--- src/prune.ts
+++ src/prune.ts
@@ -1,11 +1,13 @@
-import { SheetLocale } from './sheetLocale';
+import chalk from 'chalk';
+import { SheetLocale } from './sheetLocale.js';
+import { truncateKey } from './utils.js';
 
 export function pruneSheetLocale(sheet_locale: SheetLocale) {
   for (const entry of sheet_locale.entries) {
     if (!entry.has_visited && entry.values.used !== 'FALSE') {
       entry.values.used = 'FALSE';
       entry.has_changed = true;
-      console.log('Pruning entry', sheet_locale.getIndexKey(entry));
+      console.log(chalk.red('- pruning'), truncateKey(sheet_locale.getIndexKey(entry)));
     }
   }
 }
